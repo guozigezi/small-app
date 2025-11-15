8132c20a42985c6725290d512ea2ca928e6184ad
guozigezi <guozigezi@users.noreply.github.com>
2025-11-15 12:01:14 +0800

    优化评价页面渲染性能

--- miniprogram/pages/studentcheck/studentcheck.js
+++ miniprogram/pages/studentcheck/studentcheck.js
@@ -1,129 +1,157 @@
 const db=wx.cloud.database()
+
+function formatRecordOptions(optionList){
+  if(!Array.isArray(optionList)){
+    return []
+  }
+  return optionList.map(questionOptions=>{
+    if(!Array.isArray(questionOptions)){
+      return []
+    }
+    return questionOptions.map(option=>{
+      if(Array.isArray(option)){
+        return option[0]
+      }
+      return option
+    })
+  })
+}
+
+function extractFinalValue(record){
+  if(!record){
+    return undefined
+  }
+  const fallbackKeys=['finalResult','finalkind','最终结果','最终结论']
+  for(const key of fallbackKeys){
+    if(record[key]!==undefined){
+      return record[key]
+    }
+  }
+  const fuzzyKey=Object.keys(record).find(key=>{
+    return key.indexOf('最终')>-1 || key.indexOf('结')>-1
+  })
+  return fuzzyKey?record[fuzzyKey]:undefined
+}
+
+function buildEvaluationPayload({number,final,kinds,judge}){
+  const payload={}
+  const finalValue=final
+  const judgeMap=judge||{}
+  switch(number){
+    case 0:{
+      const finaltype=Array.isArray(finalValue)?finalValue.slice(0,4).join(''):finalValue
+      payload.finaltype=finaltype
+      payload.judgetext=judgeMap?judgeMap[finaltype]:undefined
+      break
+    }
+    case 1:{
+      payload.finaltype=finalValue
+      payload.judgetext=judgeMap?judgeMap[finalValue]:undefined
+      break
+    }
+    case 2:{
+      payload.finaltype=finalValue
+      payload.judgetext=judgeMap||[]
+      break
+    }
+    case 3:{
+      payload.finaltype=finalValue
+      payload.judgetext=judgeMap?judgeMap[finalValue]:undefined
+      break
+    }
+    case 4:{
+      if(Array.isArray(finalValue)){
+        payload.finaltype=finalValue
+        payload.judgetext=finalValue.map(item=>judgeMap?judgeMap[item]:undefined)
+      }
+      break
+    }
+    case 5:{
+      payload.finaltype=finalValue
+      payload.judgetext=judgeMap?judgeMap[finalValue]:undefined
+      break
+    }
+    case 6:{
+      if(Array.isArray(kinds)){
+        payload.finaltype=finalValue
+        payload.judgetext=kinds.map(kind=>judgeMap?judgeMap[kind]:undefined)
+      }
+      break
+    }
+    default:{
+      payload.finaltype=finalValue
+      payload.judgetext=undefined
+    }
+  }
+  return payload
+}
+
+function fetchRecordById(recordId){
+  if(!recordId){
+    return Promise.reject(new Error('missing record id'))
+  }
+  const collection=db.collection('record')
+  return collection.doc(recordId).get().catch(()=>{
+    return collection.where({_id:recordId}).limit(1).get().then(res=>{
+      const data=res.data&&res.data[0]?res.data[0]:null
+      return {data}
+    })
+  })
+}
+
 // pages/studentcheck/studentcheck.js
 Page({
   data: {
-avatural:[],
-choose:1
+    avatural:[],
+    choose:1,
+    record:[]
   },
 
-  onLoad(options) {
-   let info=wx.getStorageSync('userrecord')
-   console.log(info)
-   this.setData({
-     avatural:info[2]
-   })
-   
-   var option 
-  db.collection('record').where({
-     _id:info[1]
-   }).limit(999).get().then(res=>{
-     console.log(res)
-     this.setData({
-       title:res.data[0].题目,
-       answeer:res.data[0].答案,
-       score:res.data[0].得分,
-       final:res.data[0].最终结果,
-       kinds:res.data[0].题目类型,
-       judge:res.data[0].评价,
-       number:res.data[0].编号,
-       submmittime:res.data[0].提交时间
-     })
-//for(var i=0;i<this.data.length;i++)
-
-
-     let option=res.data[0].选项
-     for(var i=0;i<option.length;i++){
-      let onrecord='record['+i+']'
-      var option0=option[i]
-      for(var j=0;j<option0.length;j++){
-        let onrecord1='record1['+j+']'
-        let option1=option0[j]
-        this.setData({
-        [onrecord1]:option1[0]
-        })
+  onLoad() {
+    const info=wx.getStorageSync('userrecord')||[]
+    console.log(info)
+    this.setData({
+      avatural:info[2]||''
+    })
+    const recordId=info[1]
+    fetchRecordById(recordId).then(res=>{
+      const record=res.data
+      if(!record){
+        console.warn('未找到记录',recordId)
+        return
       }
-      this.setData({
-        [onrecord]:this.data.record1
+      const finalValue=extractFinalValue(record)
+      const formattedOptions=formatRecordOptions(record.选项)
+      const baseData={
+        title:record.题目||[],
+        answeer:record.答案||[],
+        score:record.得分,
+        final:finalValue,
+        kinds:record.题目类型||[],
+        judge:record.评价||{},
+        number:record.编号,
+        submmittime:record.提交时间||''
+      }
+      const evaluationPayload=buildEvaluationPayload({
+        number:baseData.number,
+        final:baseData.final,
+        kinds:baseData.kinds,
+        judge:baseData.judge
       })
-      this.setData({
-        record1:[]
-      })
-    }
-    if(this.data.number==0){
-      let finaltype=this.data.final[0]+this.data.final[1]+this.data.final[2]+this.data.final[3];
-      let judgetext=this.data.judge[finaltype];
-      this.setData({
-        finaltype:finaltype,
-        judgetext:judgetext
-      })
-    }
-      if(this.data.number==1){
-        let finaltype=this.data.final;
-        let judgetext=this.data.judge[finaltype];
-        this.setData({
-          finaltype:finaltype,
-          judgetext:judgetext
-        })
-    }
-      if(this.data.number==2){
-        // 处理评价类型的数据显示
-        console.log('评价数据:', this.data.judge);
-        this.setData({
-          finaltype: this.data.final, // 最终结果（高分/中等/低分）
-          judgetext: this.data.judge || [] // 评价文本数组
-        });
-      }
-      if(this.data.number==3){
-        this.setData({
-          finaltype:this.data.final,
-          judgetext:this.data.judge[this.data.final]
-        })
-
-      }
-      if(this.data.number==4){
-        var judgetext=[]
-        for(var i=0;i<this.data.final.length;i++){
-         
-          judgetext[i]=this.data.judge[this.data.final[i]]
-        }
-        this.setData({
-          finaltype:this.data.final,
-          judgetext:judgetext
-        })
-
-      }
-      if(this.data.number==5){
-        let judgetext=this.data.judge[this.data.final]
-        this.setData({
-          finaltype:this.data.final,
-          judgetext:judgetext
-        })
-
-      }
-      if(this.data.number==6){
-        var judge=[]
-        for(var i=0;i<this.data.kinds.length;i++){
-          judge[i]=this.data.judge[this.data.kinds[i]]
-        }
-        this.setData({
-          judge:judge
-        })
-
-      }
-   })
+      this.setData(Object.assign({},baseData,evaluationPayload,{
+        record:formattedOptions
+      }))
+    }).catch(err=>{
+      console.error('获取记录失败',err)
+    })
   },
-  choose(e)
-  {
-      let choice=e.currentTarget.dataset.text
-      if(choice==1)
-      {
-        this.setData({
-          choose:1
-        })
-      }else{
-        this.setData({
-          choose:2
-        })
-      }
+  choose(e){
+    const choice=Number(e.currentTarget.dataset.text)||1
+    if(this.data.choose===choice){
+      return
+    }
+    this.setData({
+      choose:choice
+    })
   }
 })
